77b1b0a99030b37d42bf68818333709c8b54f80f
Praveen Rawat <rawatpraveen1000@gmail.com>
2018-01-14 14:52:41 -0600

    correcting the order.

--- src/main/java/com/example/LogViewerApp/LogViewerAppApplication.java
+++ src/main/java/com/example/LogViewerApp/LogViewerAppApplication.java
@@ -3,7 +3,6 @@ package com.example.LogViewerApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
